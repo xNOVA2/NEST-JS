0e67cf61b254d0c3d6790ce9b4f44f4733c95687
ali usaid <aliusaid55@gmail.com>
2024-08-11 15:00:57 -0700

    feat: Add socket.io and websockets support to user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,14 @@
+// src/user/user.module.ts
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';  // Correct import for MongooseModule
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { User } from './user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: User }]), 
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
